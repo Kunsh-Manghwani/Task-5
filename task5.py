5c95a2381fde9ee62bc079934cc27efaa269526a
isha1206 <70023191+isha1206@users.noreply.github.com>
2020-10-19 23:45:50 +0530

    Update task5.py

--- task5.py
+++ task5.py
@@ -1,5 +1,5 @@
 
-Perform ‘Exploratory Data Analysis’ on the provided dataset ‘SampleSuperstore’ You are the business owner of the retail firm and want to see how your company is performing. You are interested in finding out the weak areas where you can work to make more profit. What all business problems you can derive by looking into the data?
+"""Perform ‘Exploratory Data Analysis’ on the provided dataset ‘SampleSuperstore’ You are the business owner of the retail firm and want to see how your company is performing. You are interested in finding out the weak areas where you can work to make more profit. What all business problems you can derive by looking into the data?"""
 
 import pandas.util.testing as tm
 import numpy as np
